87d663f43afbce01ed340d21e8358a555c5e7dd2
RV201 <raksh29rs@gmail.com>
2025-04-21 22:09:14 +0530

    Add HEllo.java code

---
+++ hello.java
@@ -0,0 +1,13 @@
+class hello{
+    public static void main(String a[]){
+        int n=5;
+        short sh = 596;
+        long l=5896l;
+        float f=5.3f;
+        double d=5.3;
+        char c='a';
+        boolean b= true;
+
+        System.out.println("Hello World");
+    }
+}
