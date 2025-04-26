18298b7b7574692d8b07f4c91ea9f31ebb4dfc0e
RV201 <raksh29rs@gmail.com>
2025-04-27 00:08:39 +0530

    Added Logical Operators

---
+++ logicalop.java
@@ -0,0 +1,14 @@
+
+
+public class logicalop {
+    public static void main(String args[]){
+        int a=5;
+        int b=5;
+        double c=6.8;
+        double d = 16.8;
+        boolean result = (a==b||c<d);
+        System.out.println(result);
+
+    }
+    
+}
