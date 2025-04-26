492731352efeb88865be7001d8225e1fb729086d
RV201 <raksh29rs@gmail.com>
2025-04-26 23:31:39 +0530

    Added Increment Code

---
+++ increment.java
@@ -0,0 +1,8 @@
+class Increment{
+    public static void main(String args[]){
+        int num = 5;
+        int result = ++num;
+        int result2 = num++;
+        System.out.print(result2);
+    }
+}
