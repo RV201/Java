2aaff6601ee1c6ece9b895824c29143143d1d8fe
RV201 <raksh29rs@gmail.com>
2025-04-28 08:30:37 +0530

    Added one more  logical operator

---
+++ logicalop2.java
@@ -0,0 +1,17 @@
+
+
+public class logicalop2 {
+    public static void main(String args[]){
+        int a=5;
+        int b=5;
+        double c=6.8;
+        double d = 16.8;
+        boolean result = (a==b&&c>d);
+        boolean result1 = (a!=b || c<d);
+        System.out.println(result);
+        System.out.println(result1);
+
+
+    }
+    
+}
